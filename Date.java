6f406100bb1d811e7bf79824762c22dbcaf7440c
derkachartem-byte <derkach.artem@student.ccm.edu>
2026-09-22 17:03:00 -0400

    Not Complete Yet

--- Date.java
+++ Date.java
@@ -1,3 +1,93 @@
 public class Date {
-    
+    private int month;
+    private int day;
+    private int year;
+    public Date(int month, int day, int year){ 
+        this.month = month; 
+        this.year = year;
+        this.day = day; 
+    }
+    public static void displayNumeric(int month, int day, int year){
+        System.out.print(month + "/" + day + "/" + Integer.toString(year));
+    }
+    public static void displayMonthFirst(int month, int day, int year){
+       String stringMonth="";
+        if(month == 1){
+            stringMonth = "January";
+        }
+        else if (month == 2){
+            stringMonth = "February";
+        }
+        else if (month == 3){
+            stringMonth = "March";
+        }
+        else if (month == 4){
+            stringMonth = "April";
+        }
+        else if (month == 5){
+            stringMonth = "May";
+        }
+        else if (month == 6){
+            stringMonth = "June";
+        }
+        else if (month == 7){
+            stringMonth = "July";
+        }
+        else if (month == 8){
+            stringMonth = "August";
+        }
+        else if (month == 9){
+            stringMonth = "September";
+        }
+        else if (month == 10){
+            stringMonth = "October";
+        }
+        else if (month == 11){
+            stringMonth = "November";
+        }
+        else if (month == 12){
+            stringMonth = "December";
+        }
+        System.out.print(stringMonth + " " + day + ", " + year);
+    }
+    public static void displayDayFirst(int month, int day, int year){
+        String stringMonth="";
+        if(month == 1){
+            stringMonth = "January";
+        }
+        else if (month == 2){
+            stringMonth = "February";
+        }
+        else if (month == 3){
+            stringMonth = "March";
+        }
+        else if (month == 4){
+            stringMonth = "April";
+        }
+        else if (month == 5){
+            stringMonth = "May";
+        }
+        else if (month == 6){
+            stringMonth = "June";
+        }
+        else if (month == 7){
+            stringMonth = "July";
+        }
+        else if (month == 8){
+            stringMonth = "August";
+        }
+        else if (month == 9){
+            stringMonth = "September";
+        }
+        else if (month == 10){
+            stringMonth = "October";
+        }
+        else if (month == 11){
+            stringMonth = "November";
+        }
+        else if (month == 12){
+            stringMonth = "December";
+        }
+        System.out.print(day + " " + stringMonth + " " + year);
+    }
 }
--- DateTest.java
+++ DateTest.java
@@ -1,3 +1,22 @@
-public class DateTest {
-    
+import java.util.Scanner;
+public class DateTest{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int month = 0, day = 0, year = 0;
+        do {
+            System.out.println("Enter the Month in number form: ");
+            month = input.nextInt();
+            System.out.println("Enter the day: ");
+            day = input.nextInt();
+            System.out.println("Enter the year: ");
+            year = input.nextInt();
+            if (month <= 12 & month >= 1 & day >= 1 & day <= 31){
+                System.out.print("```");
+                displayNumeric(month,day,year);
+                displayMonthFirst(month,day,year);
+                displayDayFirst(month,day,year);
+                System.out.print("```")
+            }
+            }while(month <= 12 & month >= 1 & day >= 1 & day <= 31);
+    }
 }
